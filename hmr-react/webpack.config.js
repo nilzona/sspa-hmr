ee4f5c56c88c0903cf042bd24dfbb18a5423d624
Anders Nilsson <nilssonanders79@gmail.com>
2021-09-07 16:41:38 +0200

    webpack config library

--- hmr-react/webpack.config.js
+++ hmr-react/webpack.config.js
@@ -18,8 +18,12 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    // output: {
+    //   library: "@hmr/react",
+    // },
     plugins: [
       new ReactRefreshWebpackPlugin({
+        // library: "@hmr/react",
         overlay: {
           sockHost: "localhost",
           sockPort: 8500,
