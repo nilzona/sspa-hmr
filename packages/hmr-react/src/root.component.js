2de55dd62c56fb6103f54d967beb1733f2a18449
Anders Nilsson <nilssonanders79@gmail.com>
2021-09-14 10:17:35 +0200

    add react libraries

--- packages/hmr-react/src/root.component.js
+++ packages/hmr-react/src/root.component.js
@@ -1,3 +1,3 @@
 export default function Root(props) {
-  return <section>{props.name} is mounted, and HMR is not working!</section>;
+  return <section>{props.name} is mounted, and HMR is NOT working!</section>;
 }
